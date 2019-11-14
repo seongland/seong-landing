9d66cba54db99a39f28b1be0f36a5fe5ba429ba0
seonglaecho <sungle3737@gmail.com>
2019-11-15 00:23:06 +0900

    🤷‍♀️proton with error

--- src/pages/meta.js
+++ src/pages/meta.js
@@ -1,19 +1,23 @@
 import React from 'react'
+import { Helmet } from "react-helmet";
 import MetaTags from 'react-meta-tags';
- 
+
 export default class Meta extends React.Component {
   render() {
     return (
-        <div className="wrapper">
-          <MetaTags>
-            <title>Seong-Land</title>
-            <meta property="og:url" content="https://www.seonglae.com/"/>
-            <meta property="og:image" content="../images/ogtag.png"/>
-            <meta property="og:title" content="Seong-Land"/>
-            <meta property="og:description" content="Land Will Guide You to know Seong-lae"/>
-            <link rel="icon" type="image/png" content="../images/favicon.png"/>
-          </MetaTags>
-        </div>
-      )
+      <div className="wrapper">
+        <MetaTags>
+          <meta charSet="utf-8" />
+          <title>Seong-Land</title>
+          <meta property="og:url" content="https://www.seonglae.com/" />
+          <meta property="og:image" content="../images/ogtag.png" />
+          <meta property="og:title" content="Seong-Land" />
+          <meta property="og:description" content="Land Will Guide You to know Seong-lae" />        </MetaTags>
+        <Helmet>
+          <link rel="canonical" href="http://mysite.com/example" />
+          <link rel="icon" type="image/png" content="../images/favicon.png" />
+        </Helmet>
+      </div>
+    )
   }
 }
