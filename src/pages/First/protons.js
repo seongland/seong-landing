4173831c755b6519dc53c6e0ab6441caed718c05
seonglaecho <sungle3737@gmail.com>
2019-11-18 00:50:04 +0900

    😎What a Clean Code

--- src/pages/First/protons.js
+++ src/pages/First/protons.js
@@ -2,91 +2,101 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Stats from "stats.js";
 import Proton from "proton-engine";
+import { RAFManager } from './manager.js' 
 
-let stats, canvas, context, proton, renderer, emitter;
+let stats, canvas, context, proton, renderer, emitter, velocity, span;
 
 export default class Protons extends React.Component {
   componentDidMount() {
     // meta - Three
     proton = ReactDOM.findDOMNode(this.refs.proton)
-    make_proton()
     window.addEventListener('resize', this.change_proton);
+    this.make();
+  }
 
-    // meta - first make
-    function make_proton() {
-      initCanvas();
-      initStats();
-      createProton();
-      render();
-    }
-
-    function initCanvas() {
-      canvas = document.getElementById("canvas");
-      canvas.width = window.innerWidth;
-      canvas.height = 3 * canvas.width;
-      context = canvas.getContext("2d");
-
-      window.onresize = function (e) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        emitter.p.x = canvas.width / 2;
-        emitter.p.y = canvas.height / 2;
-      };
-    }
-
-    function initStats() {
-      stats = new Stats();
-      stats.setMode(2);
-      stats.domElement.style.position = "absolute";
-      stats.domElement.style.left = "0px";
-      stats.domElement.style.top = "0px";
-      document.body.appendChild(stats.domElement);
-    }
-
-    function createProton() {
-      proton = new Proton();
-      emitter = new Proton.Emitter();
-      emitter.rate = new Proton.Rate(
-        new Proton.Span(10, 30),
-        new Proton.Span(0.1, 0.3)
-      );
-      emitter.addInitialize(new Proton.Mass(100));
-      emitter.addInitialize(new Proton.Radius(3, 12));
-      emitter.addInitialize(new Proton.Life(1.5, 3));
-      emitter.addInitialize(
-        new Proton.Velocity(
-          new Proton.Span(canvas.width / 300, canvas.width / 300),
-          new Proton.Span(-300, 300),
-          "polar"
-        )
-      );
-      emitter.addBehaviour(new Proton.RandomDrift(30, 30, 0.05));
-      emitter.addBehaviour(
-        new Proton.Color("ffffff", "random", Infinity, Proton.easeOutQuart)
-      );
-      emitter.addBehaviour(new Proton.Scale(1, 0.7));
-      emitter.p.x = canvas.width / 2;
-      emitter.p.y = canvas.height / 2;
-      emitter.emit();
-
-      proton.addEmitter(emitter);
-      renderer = new Proton.CanvasRenderer(canvas);
-      renderer.onProtonUpdate = () => {
-        context.fillStyle = "rgba(53, 53, 53, 0.4)";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-      };
-      proton.addRenderer(renderer);
-    }
+  make=() =>{
+    this.initCanvas();
+    this.initStats();
+    this.createProton();
+    render();
 
     function render() {
       RAFManager.add(() => {
         stats.begin();
-        emitter.rotation += 10;
         proton.update();
         stats.end();
       });
     }
+  }
 
+  initCanvas=() =>{
+    canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = 3 * canvas.width;
+    context = canvas.getContext("2d");
+
+    window.onresize = function (e) {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      emitter.p.x = canvas.width / 2;
+      emitter.p.y = canvas.height / 2;
+    };
+  }
+
+  initStats=()=> {
+    stats = new Stats();
+    stats.setMode(2);
+    stats.domElement.style.position = "absolute";
+    stats.domElement.style.left = "0px";
+    stats.domElement.style.top = "0px";
+    document.body.appendChild(stats.domElement);
+  }
+
+  createProton=() =>{
+    proton = new Proton();
+    emitter = new Proton.Emitter();
+    emitter.rate = new Proton.Rate(
+      new Proton.Span(10, 30),
+      new Proton.Span(0.1, 0.3)
+    );
+    emitter.addInitialize(new Proton.Mass(100));
+    emitter.addInitialize(new Proton.Radius(3, 12));
+    emitter.addInitialize(new Proton.Life(1, 1.2));
+    span = new Proton.Span()
+    this.change_span();
+    
+    velocity = new Proton.Velocity(
+      span,
+      new Proton.Span(-300, 300),
+      "polar"
+    )
+    emitter.addInitialize(velocity);
+    emitter.addBehaviour(new Proton.RandomDrift(30, 30, 0.05));
+    emitter.addBehaviour(
+      new Proton.Color("ffffff", "random", Infinity, Proton.easeOutQuart)
+    );
+    emitter.addBehaviour(new Proton.Scale(1, 0.7));
+    emitter.p.x = canvas.width / 2;
+    emitter.p.y = canvas.height / 2;
+    emitter.emit();
+
+    proton.addEmitter(emitter);
+    renderer = new Proton.CanvasRenderer(canvas);
+    renderer.onProtonUpdate = () => {
+      context.fillStyle = "rgba(53, 53, 53, 0.4)";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    };
+    proton.addRenderer(renderer);
+  }
+
+  change_span = () => {
+    if (canvas.width > canvas.height){
+      span.a = 1
+      span.b = 1
+      return
+    }
+    span.a = canvas.height / 300
+    span.b = canvas.height / 300
   }
 
   // meta - when window changed
@@ -95,6 +105,7 @@ export default class Protons extends React.Component {
     canvas.height = canvas.width * 3;
     emitter.p.x = canvas.width / 2;
     emitter.p.y = canvas.height / 2;
+    this.change_span();
   }
 
   componentWillUnmount() {
@@ -107,78 +118,3 @@ export default class Protons extends React.Component {
     );
   }
 }
-
-var RAFManager = {
-  timer: 0,
-  state: 'stop',
-  animations: [],
-
-  add: function add(callback) {
-    var fps = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 60;
-    var param = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
-
-    var n = 60 / fps;
-    var aniData = { callback: callback, fps: fps, n: n, param: param, i: 0 };
-    this.animations.push(aniData);
-    if (this.animations.length >= 1) this.start();
-
-    return this;
-  },
-  getIndex: function getIndex(callback) {
-    for (var i = 0; i < this.animations.length; i++) {
-      var aniData = this.animations[i];
-      if (aniData.callback === callback) return i;
-    }
-
-    return -1;
-  },
-  remove: function remove(callback) {
-    var index = this.getIndex(callback);
-    if (index < 0) return;
-
-    this.deleteMap(callback);
-    if (this.animations.length === 0) this.stop();
-
-    return this;
-  },
-  deleteMap: function deleteMap(callback) {
-    var index = this.getIndex(callback);
-    var aniData = this.animations[index];
-    for (var key in aniData) {
-      delete aniData[key];
-    } this.animations.splice(index, 1);
-  },
-  start: function start() {
-    if (this.state === 'start') return;
-
-    this.state = 'start';
-    this.tick();
-    return this;
-  },
-  stop: function stop() {
-    if (this.state === 'stop') return;
-
-    this.state = 'stop';
-    cancelAnimationFrame(this.timer);
-    return this;
-  },
-  tick: function tick() {
-    var _this = this;
-
-    this.timer = requestAnimationFrame(function () {
-      _this.tick();
-    });
-
-    for (var i = 0; i < this.animations.length; i++) {
-      var aniData = this.animations[i];
-      var callback = aniData.callback;
-      var param = aniData.param;
-
-      aniData.i++;
-      if (aniData.i >= aniData.n) {
-        callback(param);
-        aniData.i = 0;
-      }
-    }
-  }
-};
