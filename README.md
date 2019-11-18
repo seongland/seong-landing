437aa425f4d02aefa146beb8f1aad79c6850f541
seonglaecho <sungle3737@gmail.com>
2019-11-18 23:13:27 +0900

    😜refactoring and readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # portfolio
-Seonglae Land
+Seonglae Land React
+
+[Information Page](https://www.notion.so/seongland/React-f5cea60b97764daca316202077e45b1d)
--- src/pages/meta.js
+++ src/pages/meta.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Helmet } from "react-helmet";
 import MetaTags from 'react-meta-tags';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 export default class Meta extends React.Component {
   render() {
@@ -14,10 +14,11 @@ export default class Meta extends React.Component {
           <meta property="og:title" content="Seong-Land" />
           <meta property="og:description" content="Land Will Guide You to know Seong-lae" />
         </MetaTags>
-        <Helmet>
-          <link rel="canonical" href="http://mysite.com/example" />
-          <link rel="icon" type="image/png" content="../images/favicon.png" />
-        </Helmet>
+        <HelmetProvider>
+          <Helmet>
+            <link rel="icon" type="image/png" content="../images/favicon.png" />
+          </Helmet>
+        </HelmetProvider>
       </div>
     )
   }
