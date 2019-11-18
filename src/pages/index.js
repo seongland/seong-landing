81838f468ca1c7a923693d4c16f342283e899031
seonglaecho <sungle3737@gmail.com>
2019-11-19 05:57:09 +0900

    💖fantastic night all day good night with perfect og tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,12 +14,14 @@ const IndexPage = () => (
           ]}>
       <meta charSet="utf-8" />
       <title>Seong-Land</title>
+      <meta property="fb:app_id" content="419108182355029" />
       <meta property="og:site_name" content="Seong-Land" />
       <meta property="og:url" content="https://www.seonglae.com/" />
       <meta property="og:image" content={ogtag} />
       <meta property="og:title" content="Seong-Land" />
       <meta property="og:description" content="Land Will Guide You to know Seong-lae" />
       <meta property="og:locale" content="ko_KR" />
+      <meta property="og:type" content="website" />
     </OG>
     <Paral />
   </div>
