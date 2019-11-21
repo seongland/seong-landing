a9a4e15fee1dbcaf418980ecfdaaae616be83e2d
seonglaecho <sungle3737@gmail.com>
2019-11-21 21:29:51 +0900

    💕remove var and semi colone and substitute old for

--- src/components/First/protons.js
+++ src/components/First/protons.js
@@ -1,79 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Proton from "proton-engine";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Proton from "proton-engine"
 import { RAFManager } from './manager.js' 
 
-let canvas, context, proton, renderer, emitter, velocity, span;
+let canvas, context, proton, renderer, emitter, velocity, span
 
 export default class Protons extends React.Component {
   componentDidMount() {
     // meta - Three
     proton = ReactDOM.findDOMNode(this.refs.proton)
-    window.addEventListener('resize', this.change_proton);
-    this.make();
+    window.addEventListener('resize', this.change_proton)
+    this.make()
   }
 
   make=() =>{
-    this.initCanvas();
-    this.createProton();
-    render();
+    this.initCanvas()
+    this.createProton()
+    render()
 
     function render() {
       RAFManager.add(() => {
-        proton.update();
-      });
+        proton.update()
+      })
     }
   }
 
   initCanvas=() =>{
-    canvas = document.getElementById("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = 3 * canvas.width;
-    context = canvas.getContext("2d");
+    canvas = document.getElementById("canvas")
+    canvas.width = window.innerWidth
+    canvas.height = 3 * canvas.width
+    context = canvas.getContext("2d")
 
     window.onresize = function (e) {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-      emitter.p.x = canvas.width / 2;
-      emitter.p.y = canvas.height / 2;
-    };
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+      emitter.p.x = canvas.width / 2
+      emitter.p.y = canvas.height / 2
+    }
   }
 
   createProton=() =>{
-    proton = new Proton();
-    emitter = new Proton.Emitter();
+    proton = new Proton()
+    emitter = new Proton.Emitter()
     emitter.rate = new Proton.Rate(
       new Proton.Span(10, 30),
       new Proton.Span(0.1, 0.3)
-    );
-    emitter.addInitialize(new Proton.Mass(100));
-    emitter.addInitialize(new Proton.Radius(3, 12));
-    emitter.addInitialize(new Proton.Life(1, 1.2));
+    )
+    emitter.addInitialize(new Proton.Mass(100))
+    emitter.addInitialize(new Proton.Radius(3, 12))
+    emitter.addInitialize(new Proton.Life(1, 1.2))
     span = new Proton.Span()
-    this.change_span();
+    this.change_span()
     
     velocity = new Proton.Velocity(
       span,
       new Proton.Span(-300, 300),
       "polar"
     )
-    emitter.addInitialize(velocity);
-    emitter.addBehaviour(new Proton.RandomDrift(30, 30, 0.05));
+    emitter.addInitialize(velocity)
+    emitter.addBehaviour(new Proton.RandomDrift(30, 30, 0.05))
     emitter.addBehaviour(
       new Proton.Color("ffffff", "random", Infinity, Proton.easeOutQuart)
-    );
-    emitter.addBehaviour(new Proton.Scale(1, 0.7));
-    emitter.p.x = canvas.width / 2;
-    emitter.p.y = canvas.height / 2;
-    emitter.emit();
+    )
+    emitter.addBehaviour(new Proton.Scale(1, 0.7))
+    emitter.p.x = canvas.width / 2
+    emitter.p.y = canvas.height / 2
+    emitter.emit()
 
-    proton.addEmitter(emitter);
-    renderer = new Proton.CanvasRenderer(canvas);
+    proton.addEmitter(emitter)
+    renderer = new Proton.CanvasRenderer(canvas)
     renderer.onProtonUpdate = () => {
-      context.fillStyle = "rgba(53, 53, 53, 0.4)";
-      context.fillRect(0, 0, canvas.width, canvas.height);
-    };
-    proton.addRenderer(renderer);
+      context.fillStyle = "rgba(53, 53, 53, 0.4)"
+      context.fillRect(0, 0, canvas.width, canvas.height)
+    }
+    proton.addRenderer(renderer)
   }
 
   change_span = () => {
@@ -88,20 +88,20 @@ export default class Protons extends React.Component {
 
   // meta - when window changed
   change_proton = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = canvas.width * 3;
-    emitter.p.x = canvas.width / 2;
-    emitter.p.y = canvas.height / 2;
-    this.change_span();
+    canvas.width = window.innerWidth
+    canvas.height = canvas.width * 3
+    emitter.p.x = canvas.width / 2
+    emitter.p.y = canvas.height / 2
+    this.change_span()
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.change_proton);
+    window.removeEventListener('resize', this.change_proton)
   }
 
   render() {
     return (
       <canvas id="canvas" ref='proton'></canvas>
-    );
+    )
   }
 }
