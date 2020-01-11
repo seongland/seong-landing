bd4a4990650ce5584dbef00eb69289a00624f695
seonglaecho <sungle3737@gmail.com>
2020-01-11 12:50:23 +0900

    lint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
-
-import React from 'react'
-import 'react-hot-loader'
-import '../components/paral/paral.css'
-import Paral from '../components/paral/index.js'
-import favicon from '../images/favicon.png'
+import React from "react"
+import "react-hot-loader"
+import "../components/paral/paral.css"
+import Paral from "../components/paral/index.js"
+import favicon from "../images/favicon.png"
 import ogtag from "../images/ogtag.png"
-import { Helmet as OG } from 'react-helmet'
+import { Helmet as OG } from "react-helmet"
 
 const IndexPage = () => (
   <div>
-    <OG link={[
-            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
-          ]}>
+    <OG
+      link={[{ rel: "shortcut icon", type: "image/png", href: `${favicon}` }]}
+    >
       <meta charSet="utf-8" />
       <title>Seong-Land</title>
       <meta property="fb:app_id" content="419108182355029" />
@@ -19,7 +18,10 @@ const IndexPage = () => (
       <meta property="og:url" content="https://www.seonglae.com/" />
       <meta property="og:image" content={ogtag} />
       <meta property="og:title" content="Seong-Land" />
-      <meta property="og:description" content="Land Will Guide You to know Seong-lae" />
+      <meta
+        property="og:description"
+        content="Land Will Guide You to know Seong-lae"
+      />
       <meta property="og:locale" content="ko_KR" />
       <meta property="og:type" content="website" />
     </OG>
