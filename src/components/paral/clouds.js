0ad9f689c032a88ba15ea9e7ef16ee98fa5827da
seonglaecho <sungle3737@gmail.com>
2020-08-29 18:48:38 +0900

    spin the earch

--- src/components/paral/clouds.js
+++ src/components/paral/clouds.js
@@ -1,9 +1,11 @@
 import React from 'react'
+
 const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
+
 
 const Clouds = () => (
   <div>
-  {/* meta - clouds */}
+    {/* meta - clouds */}
     <img src={url('cloud')}
       style={{ display: 'block', width: '20%', marginLeft: '55%' }} alt='cloud' />
     <img src={url('cloud')}
