8411eb68448def6626c59b6969f1693829cf59f3
seonglae <sungle3737@gmail.com>
2020-12-17 02:03:40 +0900

    😍card perfect sizing

--- src/components/paral/Forth/cards.js
+++ src/components/paral/Forth/cards.js
@@ -5,6 +5,7 @@ import { useSprings, animated, interpolate } from "react-spring"
 import { useDrag } from "react-use-gesture"
 import "../cards.css"
 import Card from "react-animated-3d-card"
+import { to, from, trans } from "../../../module"
 
 const cards = [
   "https://www.linkedin.com/in/sungle3737",
@@ -20,204 +21,215 @@ const colors = [
 
 const texts = [["LinkedIn"], ["Instagram"], ["Facebook"]]
 
-const to = (i) => ({
-  x: 0,
-  y: i * -4,
-  scale: 1,
-  rot: -5 + Math.random() * 10,
-  delay: i * 100,
-})
-
-const from = (i) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
-
-const trans = (r, s) =>
-  `perspective(1500px) rotateX(10deg) rotateY(${
-    r / 10
-  }deg) rotateZ(${r}deg) scale(${s})`
-
-function Deck() {
-  const [gone] = useState(() => new Set())
-  const [props, set] = useSprings(cards.length, (i) => ({
-    ...to(i),
-    from: from(i),
-  }))
-
-  const bind = useDrag(
-    ({ args: [index], down, delta: [xDelta], direction: [xDir], velocity }) => {
-      const trigger = velocity > 0.2
-      const dir = xDir < 0 ? -1 : 1
-      if (!down && trigger) gone.add(index)
-      set((i) => {
-        if (index !== i) return
-        const isGone = gone.has(index)
-        const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0
-        const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0)
-        const scale = down ? 1.1 : 1
-        return {
-          x,
-          rot,
-          scale,
-          delay: undefined,
-          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
-        }
-      })
-      if (!down && gone.size === cards.length)
-        setTimeout(() => gone.clear() || set((i) => to(i)), 600)
-    }
-  )
-
-  return props.map(({ x, y, rot, scale }, i) => (
-    <animated.div
-      key={i}
-      style={{
-        transform: interpolate(
-          [x, y],
-          (x, y) => `translate3d(${x}px,${y}px,0)`
-        ),
-      }}
-    >
-      <animated.div
-        {...bind(i)}
-        style={{
-          transform: interpolate([rot, scale], trans),
-        }}
-      >
-        <div>
-          {cards[i].includes("vizydrop") ? (
-            <iframe
-              style={{
-                height: "50vh",
-                width: "80%",
-                marginLeft: "10%",
-                marginRight: "10%",
-                border: "none",
-              }}
-              src={cards[i]}
-            ></iframe>
-          ) : (
-            <Card
-              style={{
-                background: `linear-gradient(to right, ${colors[i][0]},${colors[i][1]}, ${colors[i][2]})`,
-                width: "80vw",
-                height: "30vh",
-                cursor: "pointer",
-              }}
-              onClick={(e) => {
-                if (!window.dragging) window.open(cards[i])
-              }}
-            >
-              <div>
-                <img
-                  style={{
-                    position: "absolute",
-                    left: "25px",
-                    top: "25px",
-                    height: "50px",
-                  }}
-                  src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
-                ></img>
-                <img
-                  style={{
-                    position: "absolute",
-                    right: "25px",
-                    top: "25px",
-                    height: "50px",
-                    filter: `brightness(${
-                      colors[i][1] === "#ffffff" ? 0.5 : 1
-                    })`,
-                  }}
-                  src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png"
-                ></img>
-              </div>
-              <div
-                style={{
-                  height: "100%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <div
-                  style={{
-                    fontSize: "30px",
-                    fontFamily: "Fira Code",
-                    color: colors[i][3],
-                  }}
-                >
-                  <label style={{ fontWeight: "bold" }}>{texts[i][0]}</label>
-                  <label style={{ marginLeft: "15px", fontWeight: "bold" }}>
-                    {texts[i][1]}
-                  </label>
-                  <label style={{ marginLeft: "15px", fontWeight: "bold" }}>
-                    {texts[i][2]}
-                  </label>
-                  <label style={{ marginLeft: "15px", fontWeight: "bold" }}>
-                    {texts[i][3]}
-                  </label>
-                </div>
-              </div>
-              <div>
-                <label
-                  style={{
-                    color: colors[i][3],
-                    position: "absolute",
-                    bottom: "60px",
-                    left: "25px",
-                    opacity: 0.5,
-                  }}
-                >
-                  Card holder
-                </label>
-                <label
-                  style={{
-                    color: colors[i][3],
-                    position: "absolute",
-                    bottom: "60px",
-                    right: "25px",
-                    opacity: 0.5,
-                  }}
-                >
-                  Expires
-                </label>
-              </div>
-
-              <div>
-                <label
-                  style={{
-                    color: colors[i][3],
-                    position: "absolute",
-                    bottom: "25px",
-                    left: "25px",
-                    opacity: 1,
-                    fontSize: "25px",
-                  }}
-                >
-                  Seonglae
-                </label>
-                <label
-                  style={{
-                    color: colors[i][3],
-                    position: "absolute",
-                    bottom: "25px",
-                    right: "25px",
-                    opacity: 1,
-                    fontSize: "25px",
-                  }}
-                >
-                  4ever
-                </label>
-              </div>
-            </Card>
-          )}
-        </div>
-      </animated.div>
-    </animated.div>
-  ))
+const isVertical = () => {
+  if (window.innerHeight > window.innerWidth) return true
+  return false
 }
 
 export default class Cards extends React.Component {
   componentDidMount() {
-    render(<Deck />, ReactDOM.findDOMNode(this.refs.cards))
+    render(<this.Deck />, ReactDOM.findDOMNode(this.refs.cards))
+    window.addEventListener("resize", this.changeCards)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.changeCards)
+  }
+
+  changeCards = () => {
+    const vertical = isVertical()
+    for (const card of document.getElementsByClassName(
+      "parallax-card-layers"
+    )) {
+      if (vertical) {
+        card.style.width = "80vw"
+        card.style.height = "50vw"
+      } else {
+        card.style.width = "40vw"
+        card.style.height = "25vw"
+      }
+    }
+    for (const card of document.getElementsByClassName("card-title")) {
+      if (vertical) card.style.fontSize = "5vw"
+      else card.style.fontSize = "2.5vw"
+    }
+  }
+
+  Deck() {
+    const [gone] = useState(() => new Set())
+    const [props, set] = useSprings(cards.length, (i) => ({
+      ...to(i),
+      from: from(i),
+    }))
+
+    const bind = useDrag(
+      ({
+        args: [index],
+        down,
+        delta: [xDelta],
+        direction: [xDir],
+        velocity,
+      }) => {
+        const trigger = velocity > 0.2
+        const dir = xDir < 0 ? -1 : 1
+        if (!down && trigger) gone.add(index)
+        set((i) => {
+          if (index !== i) return
+          const isGone = gone.has(index)
+          const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0
+          const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0)
+          const scale = down ? 1.1 : 1
+          return {
+            x,
+            rot,
+            scale,
+            delay: undefined,
+            config: {
+              friction: 50,
+              tension: down ? 800 : isGone ? 200 : 500,
+            },
+          }
+        })
+        if (!down && gone.size === cards.length)
+          setTimeout(() => gone.clear() || set((i) => to(i)), 600)
+      }
+    )
+
+    return props.map(({ x, y, rot, scale }, i) => (
+      <animated.div
+        key={i}
+        className="card"
+        style={{
+          transform: interpolate(
+            [x, y],
+            (x, y) => `translate3d(${x}vw,${y}vw,0)`
+          ),
+        }}
+      >
+        <animated.div
+          {...bind(i)}
+          style={{
+            transform: interpolate([rot, scale], trans),
+          }}
+        >
+          <Card
+            style={{
+              background: `linear-gradient(to right, ${colors[i][0]},${colors[i][1]}, ${colors[i][2]})`,
+              width: window.innerHeight > window.innerWidth ? "80vw" : "40vw",
+              height: window.innerHeight > window.innerWidth ? "50vw" : "25vw",
+              cursor: "pointer",
+            }}
+            onClick={(e) => {
+              if (!window.dragging) window.open(cards[i])
+            }}
+          >
+            <div>
+              <img
+                style={{
+                  position: "absolute",
+                  left: "2vw",
+                  top: "2vw",
+                  height: "4vw",
+                }}
+                src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
+              ></img>
+              <img
+                style={{
+                  position: "absolute",
+                  right: "2vw",
+                  top: "2vw",
+                  height: "4vw",
+                  filter: `brightness(${colors[i][1] === "#ffffff" ? 0.5 : 1})`,
+                }}
+                src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png"
+              ></img>
+            </div>
+            <div
+              style={{
+                height: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div
+                className="card-title"
+                style={{
+                  fontSize:
+                    window.innerHeight > window.innerWidth ? "5vw" : "2vw",
+                  fontFamily: "Malgun Gothic",
+                  color: colors[i][3],
+                }}
+              >
+                <label style={{ fontWeight: "bold" }}>{texts[i][0]}</label>
+                <label style={{ marginLeft: "1.5vw", fontWeight: "bold" }}>
+                  {texts[i][1]}
+                </label>
+                <label style={{ marginLeft: "1.5vw", fontWeight: "bold" }}>
+                  {texts[i][2]}
+                </label>
+                <label style={{ marginLeft: "1.5vw", fontWeight: "bold" }}>
+                  {texts[i][3]}
+                </label>
+              </div>
+            </div>
+            <div>
+              <label
+                style={{
+                  color: colors[i][3],
+                  position: "absolute",
+                  bottom: "4vw",
+                  left: "2vw",
+                  fontSize: "1vw",
+                  opacity: 0.5,
+                }}
+              >
+                Card holder
+              </label>
+              <label
+                style={{
+                  color: colors[i][3],
+                  position: "absolute",
+                  bottom: "4vw",
+                  right: "2vw",
+                  fontSize: "1vw",
+                  opacity: 0.5,
+                }}
+              >
+                Expires
+              </label>
+            </div>
+
+            <div>
+              <label
+                style={{
+                  color: colors[i][3],
+                  position: "absolute",
+                  bottom: "2vw",
+                  left: "2vw",
+                  opacity: 1,
+                  fontSize: "1.5vw",
+                }}
+              >
+                SEONGLAE
+              </label>
+              <label
+                style={{
+                  color: colors[i][3],
+                  position: "absolute",
+                  bottom: "2vw",
+                  right: "2vw",
+                  opacity: 1,
+                  fontSize: "1.5vw",
+                }}
+              >
+                4EVER
+              </label>
+            </div>
+          </Card>
+        </animated.div>
+      </animated.div>
+    ))
   }
 
   render() {
--- src/components/paral/Second/cards.js
+++ src/components/paral/Second/cards.js
@@ -5,12 +5,13 @@ import { useSprings, animated, interpolate } from "react-spring"
 import { useDrag } from "react-use-gesture"
 import "../cards.css"
 import Card from "react-animated-3d-card"
+import { to, from, trans } from "../../../module"
 
 const cards = ["https://github.com/sungle3737", "https://doc.seongland.com"]
 
 const colors = [
-  ["#0f0c29", "#302b63", "#24243e"],
-  ["#020202", "#333333", "#222222"],
+  ["#0f0c29", "#302b63", "#24243e", "#fff"],
+  ["#020202", "#333333", "#222222", "#fff"],
 ]
 
 const texts = [
@@ -18,86 +19,75 @@ const texts = [
   ["Personal", "Wiki"],
 ]
 
-const to = (i) => ({
-  x: 0,
-  y: i * -4,
-  scale: 1,
-  rot: -5 + Math.random() * 10,
-  delay: i * 100,
-})
+export default class Cards extends React.Component {
+  componentDidMount() {
+    function Deck() {
+      const [gone] = useState(() => new Set())
+      const [props, set] = useSprings(cards.length, (i) => ({
+        ...to(i),
+        from: from(i),
+      }))
 
-const from = (i) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
-
-const trans = (r, s) =>
-  `perspective(1500px) rotateX(10deg) rotateY(${
-    r / 10
-  }deg) rotateZ(${r}deg) scale(${s})`
-
-function Deck() {
-  const [gone] = useState(() => new Set())
-  const [props, set] = useSprings(cards.length, (i) => ({
-    ...to(i),
-    from: from(i),
-  }))
-
-  const bind = useDrag(
-    ({ args: [index], down, delta: [xDelta], direction: [xDir], velocity }) => {
-      const trigger = velocity > 0.2
-      const dir = xDir < 0 ? -1 : 1
-      if (!down && trigger) gone.add(index)
-      set((i) => {
-        if (index !== i) return
-        const isGone = gone.has(index)
-        const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0
-        const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0)
-        const scale = down ? 1.1 : 1
-        return {
-          x,
-          rot,
-          scale,
-          delay: undefined,
-          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
+      const bind = useDrag(
+        ({
+          args: [index],
+          down,
+          delta: [xDelta],
+          direction: [xDir],
+          velocity,
+        }) => {
+          const trigger = velocity > 0.2
+          const dir = xDir < 0 ? -1 : 1
+          if (!down && trigger) gone.add(index)
+          set((i) => {
+            if (index !== i) return
+            const isGone = gone.has(index)
+            const x = isGone
+              ? (200 + window.innerWidth) * dir
+              : down
+              ? xDelta
+              : 0
+            const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0)
+            const scale = down ? 1.1 : 1
+            return {
+              x,
+              rot,
+              scale,
+              delay: undefined,
+              config: {
+                friction: 50,
+                tension: down ? 800 : isGone ? 200 : 500,
+              },
+            }
+          })
+          if (!down && gone.size === cards.length)
+            setTimeout(() => gone.clear() || set((i) => to(i)), 600)
         }
-      })
-      if (!down && gone.size === cards.length)
-        setTimeout(() => gone.clear() || set((i) => to(i)), 600)
-    }
-  )
+      )
 
-  return props.map(({ x, y, rot, scale }, i) => (
-    <animated.div
-      key={i}
-      style={{
-        transform: interpolate(
-          [x, y],
-          (x, y) => `translate3d(${x}px,${y}px,0)`
-        ),
-      }}
-    >
-      <animated.div
-        {...bind(i)}
-        style={{
-          transform: interpolate([rot, scale], trans),
-        }}
-      >
-        <div>
-          {cards[i].includes("vizydrop") ? (
-            <iframe
-              style={{
-                height: "50vh",
-                width: "80%",
-                marginLeft: "10%",
-                marginRight: "10%",
-                border: "none",
-              }}
-              src={cards[i]}
-            ></iframe>
-          ) : (
+      return props.map(({ x, y, rot, scale }, i) => (
+        <animated.div
+          key={i}
+          className="card"
+          style={{
+            transform: interpolate(
+              [x, y],
+              (x, y) => `translate3d(${x}vw,${y}vw,0)`
+            ),
+          }}
+        >
+          <animated.div
+            {...bind(i)}
+            style={{
+              transform: interpolate([rot, scale], trans),
+            }}
+          >
             <Card
               style={{
                 background: `linear-gradient(to right, ${colors[i][0]},${colors[i][1]}, ${colors[i][2]})`,
-                width: "80vw",
-                height: "30vh",
+                width: window.innerHeight > window.innerWidth ? "80vw" : "40vw",
+                height:
+                  window.innerHeight > window.innerWidth ? "50vw" : "25vw",
                 cursor: "pointer",
               }}
               onClick={(e) => {
@@ -108,18 +98,21 @@ function Deck() {
                 <img
                   style={{
                     position: "absolute",
-                    left: "25px",
-                    top: "25px",
-                    height: "50px",
+                    left: "2vw",
+                    top: "2vw",
+                    height: "4vw",
                   }}
                   src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
                 ></img>
                 <img
                   style={{
                     position: "absolute",
-                    right: "25px",
-                    top: "25px",
-                    height: "50px",
+                    right: "2vw",
+                    top: "2vw",
+                    height: "4vw",
+                    filter: `brightness(${
+                      colors[i][1] === "#ffffff" ? 0.5 : 1
+                    })`,
                   }}
                   src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png"
                 ></img>
@@ -133,20 +126,22 @@ function Deck() {
                 }}
               >
                 <div
+                  className="card-title"
                   style={{
-                    fontSize: "30px",
-                    fontFamily: "Fira Code",
-                    color: "white",
+                    fontSize:
+                      window.innerHeight > window.innerWidth ? "5vw" : "2vw",
+                    fontFamily: "Malgun Gothic",
+                    color: colors[i][3],
                   }}
                 >
                   <label style={{ fontWeight: "bold" }}>{texts[i][0]}</label>
-                  <label style={{ marginLeft: "15px", fontWeight: "bold" }}>
+                  <label style={{ marginLeft: "1.5vw", fontWeight: "bold" }}>
                     {texts[i][1]}
                   </label>
-                  <label style={{ marginLeft: "15px", fontWeight: "bold" }}>
+                  <label style={{ marginLeft: "1.5vw", fontWeight: "bold" }}>
                     {texts[i][2]}
                   </label>
-                  <label style={{ marginLeft: "15px", fontWeight: "bold" }}>
+                  <label style={{ marginLeft: "1.5vw", fontWeight: "bold" }}>
                     {texts[i][3]}
                   </label>
                 </div>
@@ -154,10 +149,11 @@ function Deck() {
               <div>
                 <label
                   style={{
-                    color: "white",
+                    color: colors[i][3],
                     position: "absolute",
-                    bottom: "60px",
-                    left: "25px",
+                    bottom: "4vw",
+                    left: "2vw",
+                    fontSize: "1vw",
                     opacity: 0.5,
                   }}
                 >
@@ -165,10 +161,11 @@ function Deck() {
                 </label>
                 <label
                   style={{
-                    color: "white",
+                    color: colors[i][3],
                     position: "absolute",
-                    bottom: "60px",
-                    right: "25px",
+                    bottom: "4vw",
+                    right: "2vw",
+                    fontSize: "1vw",
                     opacity: 0.5,
                   }}
                 >
@@ -179,39 +176,34 @@ function Deck() {
               <div>
                 <label
                   style={{
-                    color: "white",
+                    color: colors[i][3],
                     position: "absolute",
-                    bottom: "25px",
-                    left: "25px",
+                    bottom: "2vw",
+                    left: "2vw",
                     opacity: 1,
-                    fontSize: "25px",
+                    fontSize: "1.5vw",
                   }}
                 >
-                  Seonglae
+                  SEONGLAE
                 </label>
                 <label
                   style={{
-                    color: "white",
+                    color: colors[i][3],
                     position: "absolute",
-                    bottom: "25px",
-                    right: "25px",
+                    bottom: "2vw",
+                    right: "2vw",
                     opacity: 1,
-                    fontSize: "25px",
+                    fontSize: "1.5vw",
                   }}
                 >
-                  4ever
+                  4EVER
                 </label>
               </div>
             </Card>
-          )}
-        </div>
-      </animated.div>
-    </animated.div>
-  ))
-}
-
-export default class Cards extends React.Component {
-  componentDidMount() {
+          </animated.div>
+        </animated.div>
+      ))
+    }
     render(<Deck />, ReactDOM.findDOMNode(this.refs.cards))
   }
 
